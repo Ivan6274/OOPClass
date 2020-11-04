c7240e90da37d0b7f1558b257bd99bb8d4e413f1
Ivan6274 <Isivanov2011@gmail.com>
2020-11-04 14:54:56 +0300

    Initial commit

---
+++ src/main/java/ru/netology/domain/NetologyPost.java
@@ -0,0 +1,52 @@
+package ru.netology.domain;
+
+public class NetologyPost {
+
+    public class WelomeLogo {
+        private String imageUrl;
+        private String text;
+    }
+
+    public class SubscribersInfo {
+        private int subscriberId;
+        private String subsciberImageUrl;
+        private int countSubdcribers;
+        private boolean userIsSubscribded;
+    }
+
+    public class InformationAboutUs {
+        private String imageUrl;
+        private String text;
+        private String adressUrl;
+        private String locationUrl;
+    }
+    public class Market {
+        private String imageUrl;
+        private String text;
+        private String coast;
+    }
+
+    public class Article{
+        private String imageUrl;
+        private String text;
+    }
+
+    public class PostsInfo{
+        private String imageUrl;
+        private String text;
+        private String dataOfWriting;
+        private int likesCount;
+        private boolean userIsLike;
+        private int repostCount;
+        private int commentsCount;
+        private int viewCount;
+        private boolean takeToFavorites;
+    }
+
+    public class CommentsInfo{
+        private String userId;
+        private String dataOfWriting;
+        private String userImageUrl;
+        private String text;
+    }
+}
