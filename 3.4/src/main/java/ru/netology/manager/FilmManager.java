a4cf0053af89598034258ba5faa080675d6278cb
Ivan6274 <isivanov2011@gmail.com>
2020-12-17 21:31:30 +0300

    Initial commit

--- 3.4/src/main/java/ru/netology/manager/FilmManager.java
+++ 3.4/src/main/java/ru/netology/manager/FilmManager.java
@@ -22,8 +22,7 @@ class FilmManager {
 
   public PurchaseItem[] getAll() {
     PurchaseItem[] result = new PurchaseItem[items.length];
-    // РїРµСЂРµР±РёСЂР°РµРј РјР°СЃСЃРёРІ РІ РїСЂСЏРјРѕРј РїРѕСЂСЏРґРєРµ
-    // РЅРѕ РєР»Р°РґС‘Рј РІ СЂРµР·СѓР»СЊС‚Р°С‚С‹ РІ РѕР±СЂР°С‚РЅРѕРј
+
     for (int i = 0; i < result.length; i++) {
       int index = items.length - i - 1;
       result[i] = items[index];
